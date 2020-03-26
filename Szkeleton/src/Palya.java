71092e3da30fa73ce9ac493aae5cd7985d49b4ef
Sándor József <Sndor Jzsef@DESKTOP-QUUTLL5>
2020-03-26 21:14:36 +0200

    palya

--- Szkeleton/src/Palya.java
+++ Szkeleton/src/Palya.java
@@ -1,4 +1,25 @@
 
 public class Palya {
-
+	private int maxJatekos;
+	private Szereplo aktJatekos;
+	private int alkatreszek;
+	
+	public void Leptet() {
+		
+	}
+	public void Hovihar() {
+		
+	}
+	public void JatekVege(boolean nyert) {
+		
+	}
+	public void JatekotKezd() {
+		
+	}
+	public void AlkatresztKezel(boolean bejelentheto) {
+		
+	}
+	public void MezokOsszekot() {
+		
+	}
 }
