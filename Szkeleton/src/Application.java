1978bb1869c7613d823f60b81f5d279dee645c9b
dominik frey <frey.dominik@hotmail.hu>
2020-03-26 21:48:08 +0100

    Dominik- Palya és Application elkezdése

--- Szkeleton/src/Application.java
+++ Szkeleton/src/Application.java
@@ -2,8 +2,39 @@
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Aviato");
-
+		int useCaseID = Integer.parseInt(System.console().readLine());
+		
 	}
-
+	
+	private static void UseCase1() {
+		
+	}
+	private static void UseCase2() {
+			
+	}
+	private static void UseCase3() {
+		
+	}
+	private static void UseCase4() {
+		
+	}
+	private static void UseCase5() {
+		
+	}
+	private static void UseCase6() {
+		
+	}
+	private static void UseCase7() {
+		
+	}
+	private static void UseCase8() {
+		
+	}
+	private static void UseCase9() {
+		
+	}
+	private static void UseCase10() {
+		
+	}
+	
 }
