0fee686f434e337cf403d3857109098f3d057a11
Sándor József <Sndor Jzsef@DESKTOP-QUUTLL5>
2020-03-26 21:42:05 +0200

    szereplo

--- Szkeleton/src/Szereplo.java
+++ Szkeleton/src/Szereplo.java
@@ -1,4 +1,53 @@
 
-public class Szereplo {
-
+public abstract class Szereplo {
+	
+	protected int lepesszam;
+	protected int testho;
+	
+	public void Atlep(Mezo cel) {
+		
+	}
+	public void Takarit() {
+		
+	}
+	public void TesthoHozzad(int novekmeny) {
+		
+	}
+	public void Felvesz() {
+		
+	}
+	public void Hasznal(Targy targy) {
+		
+	}
+	//public Mezo getMezo() {
+		
+		
+	//}
+	public void setMezo(Mezo mezo) {
+		
+	}
+	public void Eldob(Targy targy) {
+		
+	}
+	public void SpecKepesseg() {
+		
+	}
+	public void setLepesszam(int a) {
+		
+	}
+	public void Vegeztem() {
+		
+	}
+	public void setRuha(Ruha ruha) {
+		
+	}
+	public void Elazik() {
+		
+	}
+	public void Megszarit() {
+		
+	}
+	public void Meghaltam() {
+		
+	}
 }
