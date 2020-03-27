c0090c465805b52fccf6d6aa1ff52649ca0ec005
kopero <belaakos.vizi@gmail.com>
2020-03-27 20:43:40 +0100

    kotel indent

--- Szkeleton/src/objects/Kotel.java
+++ Szkeleton/src/objects/Kotel.java
@@ -1,8 +1,12 @@
 package objects;
+import indent.Indentor;
 
 public class Kotel implements Targy{
 	public void Kepesseg(Szereplo sz) {
+		Indentor.incLevel();
+		System.out.println(Indentor.getIndent() + "Kotel.Kepesseg()");
 		sz.getMezo().ValasztSzomszed().Kimenekit(sz.getMezo());
+		Indentor.degLevel();
 		return;
 	}
 }
