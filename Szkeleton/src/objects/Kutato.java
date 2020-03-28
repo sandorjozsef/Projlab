3a4708b65acd5e9914cc79e9be3ee8192689dca7
dominik frey <frey.dominik@hotmail.hu>
2020-03-28 22:47:05 +0100

    kerdes

--- Szkeleton/src/objects/Kutato.java
+++ Szkeleton/src/objects/Kutato.java
@@ -8,14 +8,38 @@ import indent.Indentor;
 
 public class Kutato extends Szereplo{
 public void SpecKepesseg(Mezo cel) {
-		/*boolean rajta=aktmezo.isSzomszed(cel);
-		if(rajta || aktmezo==cel) {
-			cel.Felderit();
-		}*/
+		Indentor.incLevel();
+		System.out.println(Indentor.getIndent()+"Kutato.SpecKepesseg()");
 		
+		
+		
+		boolean szomszed=aktmezo.isSzomszed(cel);
+		if(szomszed) {
+			cel.Felderit();
+		}
+		else {	
+			String answer1 = "";
+			while (!answer1.equals("Y") && !answer1.equals("N")) {
+				System.out.print(Indentor.getIndent()+" - A felderitendo mezo az aktuális mezo? ? (Y/N) ");
+				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				try {
+					answer1 = reader.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if (answer1.equals("Y")) {
+					cel.Felderit();
+				}
+			}
+		}
+		
+		/*
+		 * 
+		 *
 		String answer1 = "";
 		while (!answer1.equals("Y") && !answer1.equals("N")) {
-			System.out.print("Aktualis vagy szomszedos mezot akarsz felderiteni ? (Y/N) ");
+			System.out.print(Indentor.getIndent()+" - Aktualis vagy szomszedos mezot akarsz felderiteni ? (Y/N) ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			try {
 				answer1 = reader.readLine();
@@ -27,9 +51,8 @@ public void SpecKepesseg(Mezo cel) {
 				cel.Felderit();
 			}
 		}
-
-		Indentor.incLevel();
-		System.out.println(Indentor.getIndent()+"Kutato.SpecKepesseg()\n");
+		 
+		 */
 		Indentor.degLevel();
 	}
 	public void Hasznal(Targy t) {
