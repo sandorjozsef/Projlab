05b45df61dda5f574caae044d1fd8cc2739c2ae4
Ábel Mózsa <abelmo@sch.bme.hu>
2020-03-29 16:56:32 +0200

    Iglu ujrakommentelve

--- Szkeleton/src/objects/Iglu.java
+++ Szkeleton/src/objects/Iglu.java
@@ -5,19 +5,23 @@ import java.util.ArrayList;
 import indent.Indentor;
 
 public class Iglu extends Epulet{
-
+	/*
+	 *  nem csinál semmit, hiszen ha iglu van a mezõn akkor nem kell levonni
+	 */
 	@Override
 	public void Levon(ArrayList<Szereplo> szereplok) {
 		// TODO Auto-generated method stub
 		Indentor.incLevel();
 		System.out.println(Indentor.getIndent()+"Iglu.Levon()");
 		Indentor.degLevel();
-		// nem csinál semmit, hiszen ha iglu van a mezõn akkor nem kell levonni
+		
 		
 	}
+	/*
+	 *	tudatja a kiiratásnál hogy egy Iglun végzik az esetlegesen
+	 *	mezobol nem felüldefiniált függvényt	 
+	 */
 	public String Name() {
 		return "Iglu";
-		//tudatja a kiiratásnál hogy egy Iglun végzik az esetlegesen
-		//mezobol nem felüldefiniált függvényt
 	}
 }
